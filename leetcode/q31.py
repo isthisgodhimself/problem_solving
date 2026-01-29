42da0b90df3bc92935784da2a29110e419363b39
Dhananjay Ojha <dhananjayojha@Dhananjays-MacBook-Pro.local>
2026-01-30 00:28:48 +0530

    done code for q31.pt

--- leetcode/q31.py
+++ leetcode/q31.py
@@ -7,13 +7,29 @@ class solution:
         then return the first permutation
 
         """
-        result = [self.array] 
+        break_index = -1 
+        
+        #first figuring out the break index 
+        for index in range(len(array) - 2 , -1 , -1):
+            if array[index] < array[index+1] :
+                break_index == index
+                break
+        #if break index is still -1 that means its the last permutation , therefore returning the first 
+        if break_index = -1 : 
+            return array[-1:]
 
-        # lets define our code here
 
-        return result 
-
+        # then we have to swap the element with the just larger elemnt in the remaining array after the 
+        # break point 
+        for index in range(len(array)-1, index, -1):
+            if array[index] > array[break_index] : 
+                #swapping the values 
+                array[index] , array[break_index] = array[break_index] , array[index]
+                
+        # after that sorting the array after the break index 
+        return array[:break_index] + sorted(array[break_index + 1:]) 
+                                      
 if'__init__' = 'main':
     sol = solution()
-    sol.next_permutation(
+    sol.next_permutation()
 
